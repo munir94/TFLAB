f9912ad81e186e9e302d5be6b6ef44cecc5ebcbf
MunirTajudin <abdul.munir94@outlook.com>
2021-09-17 16:39:56 +0800

    add log analytic module

---
+++ modules/diagnostic/log-analytics/output.tf
@@ -0,0 +1,4 @@
+output "law-id" {
+  description = "Log Analytic ID"
+  value = azurerm_log_analytics_workspace.law.id
+}
---
+++ modules/diagnostic/log-analytics/variable.tf
@@ -0,0 +1,25 @@
+variable "law-rg" {
+    type = string
+    default = "CORE-ANALYTIC01"
+  
+}
+variable "law-loc" {
+    type = string
+    default = "southeastasia" 
+}
+
+variable "law-name" {
+    type = string
+    default = "LAW-010" 
+}
+variable "law-sku" {
+    type = string
+    default = "PerGB2018" 
+}
+variable "law-retention" { 
+    default = 30
+}
+
+
+
+
