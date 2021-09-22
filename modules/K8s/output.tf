acf46a18cc6f69e2f9e6c7a2e56f060edb626172
MunirTajudin <abdul.munir94@outlook.com>
2021-09-22 15:15:31 +0800

    Add aks mixed mode - first draft

--- modules/K8s/output.tf
+++
@@ -1,9 +0,0 @@
-output "client_certificate" {
-  value = azurerm_kubernetes_cluster.aks1.kube_config.0.client_certificate
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks1.kube_config_raw
-
-  sensitive = true
-}
