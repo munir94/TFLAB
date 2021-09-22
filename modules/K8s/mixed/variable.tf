bc863c53d8e15d3e06cab1cd4c2ebd2fa3a83aed
MunirTajudin <abdul.munir94@outlook.com>
2021-09-22 15:16:45 +0800

    Add aks mixed mode - first draft

---
+++ modules/K8s/mixed/variable.tf
@@ -0,0 +1,44 @@
+variable "client_id" {
+    default = ""
+    description = "client id for SPN"
+    sensitive = true
+}
+variable "client_sec" {
+  default = ""
+  description = "client secret for SPN"
+  sensitive = true
+}
+variable "admin" {
+    default = "muniradm"
+    description = "Admin for Linux and Windows"
+    sensitive = true
+}
+variable "ssh" {
+    default = ""
+    description = "SSH key"
+    sensitive = true
+}
+variable "winpass" {
+    default = "1a2bP@ssw0rd@1234567"
+    description = "Admin for Windows"
+    sensitive = true
+}
+variable "aks-subnet-id" {
+  type = string 
+  description = "subnet for aks deployment"
+}
+variable "aks-name" {
+  type = string 
+  description = "name for aks deployment"
+  default = "myaks"
+}
+variable "aks-dns" {
+  type = string 
+  description = "dnsname for aks deployment"
+  default = "myaks-dns"
+}
+variable "aks-version" {
+  type = string 
+  description = "name for aks deployment"
+  default = "1.20.9"
+}
