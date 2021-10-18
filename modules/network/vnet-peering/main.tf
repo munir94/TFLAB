beef691a7e492ac3f8b3ff81b05dbf349d096771
MunirTajudin <abdul.munir94@outlook.com>
2021-10-18 15:10:37 +0800

    file renaming from main

--- modules/network/vnet-peering/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_virtual_network_peering" "peering" {
-  name                      = var.peer-name
-  resource_group_name       = var.peer-rg
-  virtual_network_name      = var.source-vnet-name
-  remote_virtual_network_id = var.target-vnet-id
-  use_remote_gateways       = lower(var.use-remote-gw)
-  #depends_on                = var.vnet-creation 
-}
