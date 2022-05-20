4898320b9f1d28bdfcdd68e61207127fd65a93ae
MunirTajudin <abdul.munir94@outlook.com>
2022-05-20 12:59:48 +0800

    update traefik app

---
+++ modules/K8s/k8-traefik/testapp.tf
@@ -0,0 +1,115 @@
+
+# NGINX 1 Test Deployment
+#
+# TODO: Change your-domain according to your DNS record that you want to create
+# TODO: Change your-zone-id according to your DNS zone ID in Cloudflare
+# ---
+
+resource "kubernetes_namespace" "nginx1" {
+
+    metadata {
+        name = "nginx1"
+    }
+}
+
+
+resource "kubernetes_deployment" "nginx1" {
+
+    depends_on = [
+        kubernetes_namespace.nginx1
+    ]
+
+    metadata {
+        name = "nginx1"
+        namespace = "nginx1"
+        labels = {
+            app = "nginx1"
+        }
+    }
+
+    spec {
+        replicas = 1
+
+        selector {
+            match_labels = {
+                app = "nginx1"
+            }
+        }
+
+        template {
+            metadata {
+                labels = {
+                    app = "nginx1"
+                }
+            }
+
+            spec {
+                container {
+                    image = "nginxdemos/hello"
+                    name  = "nginx"
+
+                    port {
+                        container_port = 80
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+resource "kubernetes_service" "nginx1" {
+
+    depends_on = [
+        kubernetes_namespace.nginx1
+    ]
+
+    metadata {
+        name = "nginx1"
+        namespace = "nginx1"
+    }
+    spec {
+        selector = {
+            app = "nginx1"
+        }
+        port {
+            port = 80
+        }
+
+        type = "ClusterIP"
+    }
+}
+
+resource "kubernetes_ingress_v1" "nginx1" {
+
+    depends_on = [kubernetes_namespace.nginx1]
+
+    metadata {
+        name = "nginx1"
+        namespace = "nginx1"
+    }
+
+    spec {
+        rule {
+
+            host = "vote.munirtajudin.xyz"
+
+            http {
+
+                path {
+                    path = "/"
+
+                    backend {
+                        service {
+                            name = "nginx1"
+                            port {
+                                number = 80
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
